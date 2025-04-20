5a4b9ce0e4bc299bbc3ba68b8ba4d6fb8d6e1837
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2025-04-20 13:24:34 +0300

    Add Encapsulation.csproj with the example for BadEncapsulation.

---
+++ OOP/Encapsulation/BadEncapsulation/BankAccount.cs
@@ -0,0 +1,20 @@
+﻿namespace Encapsulation.BadEncapsulation
+{
+    public class BankAccount
+    {
+        /*Using of public fields is VERY BAD practice.
+         The problem now is that because they are public,
+         everyone from outside can change their values as
+         they want even with invalid values without any validation.*/
+
+        public decimal _balance;
+        public string? _accountNumber;
+
+        /*Technically this object can be created with invalid information.*/
+        public BankAccount(decimal balance, string accountNumber)
+        {
+            _balance = balance;
+            _accountNumber = accountNumber;
+        }
+    }
+}
---
+++ OOP/Encapsulation/Program.cs
@@ -0,0 +1,44 @@
+﻿/*============Start of Bad encapsulation example===========*/
+
+using Encapsulation.BadEncapsulation;
+
+/*Here technically we can pass any string for account number 
+ which leads to working with wrong data after that and bugs.
+ There is not validation in our class - BankAccount for the 
+ accountNumber field.*/
+
+BankAccount badBankAccount = new BankAccount(100m, "WhateverString");
+
+/*Now we set a negative value to the balance which is totally unacceptable.
+  This leads to working with invalid data in the application.
+  Imagine this data is published to official report for the customer and
+  he sees this balance...*/
+
+badBankAccount._balance = -300;
+
+/*Imagine we want some logic to be called when the balance is changed.
+ In the current situation we have to add manually this logic (ReportBalance method) after
+ the changed balance EVERY time when we change it. See the example below:*/
+
+badBankAccount._balance = 600;
+ReportBalance(badBankAccount._balance);
+
+badBankAccount._balance = 100;
+ReportBalance(badBankAccount._balance);
+
+badBankAccount._balance = 400;
+ReportBalance(badBankAccount._balance);
+
+badBankAccount._balance = 1000;
+ReportBalance(badBankAccount._balance); 
+
+/*Now imagine that you have 100 places (classes) in your codebase where you have to do that...
+ This is totally wrong. Please see the good example in the GoodEncapsulation folder.*/
+
+
+static void ReportBalance(decimal balance)
+{
+    Console.WriteLine($"The current balance for this account is: {balance}.");
+}
+
+/*============End of Bad encapsulation example===========*/
